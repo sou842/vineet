d87166ba60b8d5238950b555e1d18ce9496123c3
animesh-0041 <ghoroianimesh1508@gmail.com>
2023-07-14 16:22:36 +0530

    sign in and signup done

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
-const tara = '*';
+const express=require("express");
+const cors = require("cors");
+const { connection } = require("./db");
+const { userRoute } = require('./route/user.route');
+
+require('dotenv').config();
+const app=express()
+app.use(express.json())
+app.use(cors())
+app.use('/api',userRoute)
+
+
+
+
+app.listen(process.env.PORT,async()=>{
+    try {
+        await connection
+        console.log('conected to mongoDB'); 
+    } catch (error) {
+        console.log(error);
+    }
+    console.log(`http://localhost:${process.env.PORT}`);
+})
---
+++ backend/model/user.model.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose")
+
+
+const userSchema=mongoose.Schema({
+    name:{type: String, required: true},
+    email:{type: String, required: true},
+    mobileNumber:{type: String, required: true},
+    address:{type: String, required: true},
+    city:{type: String, required: true},
+    pincode:{type: String, required: true},
+    state:{type: String, required: true},
+    shopeName:{type: String, required: true},
+    panNumber:{type: String, required: true},
+    aadharNumber:{type: String, required: true},
+    password:{type: String, required: true},
+    vendorID:{type: String, required: true},
+
+})
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports={
+    UserModel
+}
