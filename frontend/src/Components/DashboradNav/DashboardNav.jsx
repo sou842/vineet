2cd46c96aeb4ee505e20233a26d9a319fc4470ff
sourav samanta <saifactplanet@gmail.com>
2023-07-17 01:16:25 +0530

    mee

--- frontend/src/Components/DashboradNav/DashboardNav.jsx
+++ frontend/src/Components/DashboradNav/DashboardNav.jsx
@@ -1,13 +1,14 @@
 import { useNavigate } from 'react-router-dom';
 import './DashboardNav.css'
-import { Button, Menu, MenuButton, MenuItem, MenuList, useToast } from '@chakra-ui/react';
+import { Button, Menu, MenuButton, MenuItem, MenuList, MenuGroup, useToast, useMediaQuery } from '@chakra-ui/react';
 import { ChevronDownIcon } from "@chakra-ui/icons";
 
 
 
 export const DashboardNav = () => {
-    const navigate = useNavigate()
+    const [isSmallerThan1000] = useMediaQuery("(max-width: 1000px)");
     const portalData = JSON.parse(localStorage.getItem("digitalPortal")) || null
+    const navigate = useNavigate()
     const toast = useToast()
 
 
@@ -40,35 +41,61 @@ export const DashboardNav = () => {
             </div>
 
             <div className='dashboard_0'>
-                <div>
-                    <div><a href="/Dashboard"><p>Dashboard</p></a></div>
-                    <div>
-                        <Menu >
-                            <MenuButton
-                                as={Button}
-                                rightIcon={<ChevronDownIcon />}
-                                colorScheme="#0a9cf8"
-                                margin={0}
-                            >
-                                Services
+                {isSmallerThan1000 ?
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <Menu>
+                            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} colorScheme="#0a9cf8" margin={0}>
+                                Menu
                             </MenuButton>
-                            <MenuList color={"black"} >
-                                <MenuItem onClick={() => navigate('/PanCard')}>Pan Card</MenuItem>
-                                <MenuItem onClick={() => navigate('/ChangePassword')}>Electricity</MenuItem>
-                                <MenuItem onClick={() => navigate('/profile')}>Mobile Recharge</MenuItem>
-                                <MenuItem onClick={() => navigate('/profile')}>DTH Recharge</MenuItem>
-                                <MenuItem onClick={() => navigate('/profile')}>ITR</MenuItem>
-                                <MenuItem onClick={() => navigate('/profile')}>GST</MenuItem>
-                                <MenuItem onClick={() => navigate('/profile')}>Online Job Portal</MenuItem>
-
+                            <MenuList color={"black"}>
+                                <MenuItem onClick={() => navigate('/Dashboard')}>Dashboard</MenuItem>
+                                <MenuItem onClick={() => navigate('#')}>Traning Manual</MenuItem>
+                                <MenuItem onClick={() => navigate('#')}>Downloads</MenuItem>
+                                <MenuItem onClick={() => navigate('/profile')}>Ledger</MenuItem>
+                                <MenuItem onClick={() => navigate('/profile')}>Add Money</MenuItem>
+                                <Menu placement="right-start">
+                                    <MenuButton colorScheme='white' color={'black'} as={Button} rightIcon={<ChevronDownIcon />} bg={'white'} margin={0}>Services</MenuButton>
+                                    <MenuList>
+                                        <MenuItem onClick={() => navigate('/PanCard')}>Pan Card</MenuItem>
+                                        <MenuItem onClick={() => navigate('/ChangePassword')}>Electricity</MenuItem>
+                                        <MenuItem onClick={() => navigate('/profile')}>Mobile Recharge</MenuItem>
+                                        <MenuItem onClick={() => navigate('/profile')}>DTH Recharge</MenuItem>
+                                        <MenuItem onClick={() => navigate('/profile')}>Online Job Portal</MenuItem>
+                                    </MenuList>
+                                </Menu>
                             </MenuList>
                         </Menu>
                     </div>
-                    <div><a href="#"><p>Traning Manual</p></a></div>
-                    <div><a href="#"><p>Downloads</p></a></div>
-                    <div><a href="#"><p>Ledger</p></a></div>
-                    <div><a href="#"><p>Add Money</p></a></div>
-                </div>
+                    :
+                    <div>
+                        <div><a href="/Dashboard"><p>Dashboard</p></a></div>
+                        <div>
+                            <Menu >
+                                <MenuButton
+                                    as={Button}
+                                    rightIcon={<ChevronDownIcon />}
+                                    colorScheme="#0a9cf8"
+                                    margin={0}
+                                >
+                                    Services
+                                </MenuButton>
+                                <MenuList color={"black"} >
+                                    <MenuItem onClick={() => navigate('/PanCard')}>Pan Card</MenuItem>
+                                    <MenuItem onClick={() => navigate('/ChangePassword')}>Electricity</MenuItem>
+                                    <MenuItem onClick={() => navigate('/profile')}>Mobile Recharge</MenuItem>
+                                    <MenuItem onClick={() => navigate('/profile')}>DTH Recharge</MenuItem>
+                                    <MenuItem onClick={() => navigate('/profile')}>ITR</MenuItem>
+                                    <MenuItem onClick={() => navigate('/profile')}>GST</MenuItem>
+                                    <MenuItem onClick={() => navigate('/profile')}>Online Job Portal</MenuItem>
+
+                                </MenuList>
+                            </Menu>
+                        </div>
+                        <div><a href="#"><p>Traning Manual</p></a></div>
+                        <div><a href="#"><p>Downloads</p></a></div>
+                        <div><a href="#"><p>Ledger</p></a></div>
+                        <div><a href="#"><p>Add Money</p></a></div>
+                    </div>}
                 <div>
                     <div><a href="#"><p>PCW: Rs.0</p></a></div>
                     <div><a href="#"><p>OCW: Rs.0</p></a></div>
