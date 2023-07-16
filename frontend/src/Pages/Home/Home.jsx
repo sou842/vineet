32434385accf0f1019e9fbac8672e7bede9d6c5c
sourav samanta <saifactplanet@gmail.com>
2023-07-17 02:30:44 +0530

    6

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -12,22 +12,22 @@ return (
         <div className="home_01">
             <h1>WELCOME TO VINEET INDIA PORTAL</h1>
             <div>
-                <p>Email:- helpdigitalindiaportal@gmail.com</p>
+                <p><b>Email:-</b> helpdigitalindiaportal@gmail.com</p>
                 <p>"Dear User & Visitors, please be informed that this portal is totally free of cost, no cost is asked for its registration.If anyone asks any money for the same, Please don't do so and kindly reach out to given Numbers and Email"</p>
             </div>
         </div>
         <div className="home_02">
             <h1>SERVICES</h1>
             <div>
-                <div><a href="#">PAN CARD</a></div>
-                <div><a href="#">MOBILE & DTH</a></div>
-                <div><a href="#">Electricity Bill</a></div>
-                <div><a href="#">ITR</a></div>
-                <div><a href="#">GST</a></div>
-                <div><a href="#">DIGITAL SIGNATURE</a></div>
-                <div><a href="#">UTI</a></div>
-                <div><a href="#">NATIONAL PENSION SYSTEM</a></div>
-                <div><a href="#">JOB PORTAL</a></div>
+                <div><a href="#"><p>PAN CARD</p></a></div>
+                <div><a href="#"><p>MOBILE & DTH</p></a></div>
+                <div><a href="#"><p>Electricity Bill</p></a></div>
+                <div><a href="#"><p>ITR</p></a></div>
+                <div><a href="#"><p>GST</p></a></div>
+                <div><a href="#"><p>DIGITAL SIGNATURE</p></a></div>
+                <div><a href="#"><p>UTI</p></a></div>
+                <div><a href="#"><p>NATIONAL PENSION</p></a></div>
+                <div><a href="#"><p>JOB PORTAL</p></a></div>
             </div>
         </div>
         </div>
