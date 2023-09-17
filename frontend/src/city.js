63debbe292f69d9bcec7a32ee652626fd61daf0e
animesh-0041 <ghoroianimesh1508@gmail.com>
2023-09-17 22:46:04 +0530

    done

--- frontend/src/city.js
+++ frontend/src/city.js
@@ -1,18 +1,380 @@
-export const city = ['Kanpur','Lucknow']
+export const city = ['Kanpur','Lucknow','Ghaziabad','Agra','Meerut','Varanasi','Bareilly','Aligarh','Moradabad','Saharanpur','Gorakhpur','Allahabad','Firozabad','Jhansi','Muzaffarnagar','Mathura-Vrindavan','Rampur','Shahjahanpur','Gautam Buddha Nagar','Farrukhabad-Fatehgarh','Budaun','Maunath Bhanjan','Hapur','Etawah','Mirzapur-Vindhyachal','Bulandshahr','Sambhal','Hardoi','Fatehpur','Raebareli','Orai','Sitapur','Bahraich','Modinagar','Unnao','Jaunpur','Lakhimpur','Hathras','Banda','Pilibhit','Barabanki','Khurja','Gonda','Mainpuri','Lalitpur','Etah','Deoria','Badaun','Ghazipur','Sultanpur','Azamgarh','Bijnor','Basti','Chandausi','Akbarpur','Ballia','Tanda','Greater Noida','Shikohabad','Shamli','Awagarh','Kasganj',]
 
 
 export const city_data = {
-    'Kanpur':{
-        'Area Code': 'CHE',
+    "Kanpur": {
+        'Area Code': 'DLC',
         'AO Type': 'WX',
-        'Range Code': 191,
-        'AO Number': 91
+        'Range Code': 293,
+        'AO Number': 1
     },
-    'Lucknow':{
+    "Lucknow": {
         'Area Code': 'DLC',
         'AO Type': 'CA',
         'Range Code': 266,
         'AO Number': 1
+    },
+    "Ghaziabad": {
+        'Area Code': 'DLC',
+        'AO Type': 'CA',
+        'Range Code': 257,
+        'AO Number': 1
+    },
+    "Agra": {
+        'Area Code': 'KNP',
+        'AO Type': 'C',
+        'Range Code': 75,
+        'AO Number': 1
+    },
+    "Meerut": {
+        'Area Code': 'KNP',
+        'AO Type': 'C',
+        'Range Code': 14,
+        'AO Number': 1
+    },
+    "Varanasi": {
+        'Area Code': 'DLC',
+        'AO Type': 'WX',
+        'Range Code': 264,
+        'AO Number': 2
+    },
+    "Bareilly": {
+        'Area Code': 'DLC',
+        'AO Type': 'WX',
+        'Range Code': 266,
+        'AO Number': 2
+    },
+    "Aligarh": {
+        'Area Code': 'KNP',
+        'AO Type': 'C',
+        'Range Code': 85,
+        'AO Number': 1
+    },
+    "Moradabad": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 71,
+        'AO Number': 1
+    },
+    "Saharanpur": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 23,
+        'AO Number': 95
+    },
+    "Gorakhpur": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 26,
+        'AO Number': 1
+    },
+    "Allahabad": {
+        'Area Code': 'DLC',
+        'AO Type': 'CA',
+        'Range Code': 264,
+        'AO Number': 1
+    },
+    "Firozabad": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 80,
+        'AO Number': 91
+    },
+    "Jhansi": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 80,
+        'AO Number': 94
+    },
+    "Muzaffarnagar": {
+        'Area Code': 'KNP',
+        'AO Type': 'C',
+        'Range Code': 23,
+        'AO Number': 1
+    },
+    "Mathura-Vrindavan": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 75,
+        'AO Number': 91
+    },
+    "Rampur": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 71,
+        'AO Number': 4
+    },
+    "Shahjahanpur": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 41,
+        'AO Number': 13
+    },
+    "Gautam Buddha Nagar": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 100,
+        'AO Number': 1
+    },
+    "Farrukhabad-Fatehgarhpur": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 85,
+        'AO Number': 91
+    },
+    "Budaun": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 71,
+        'AO Number': 91
+    },
+    "Maunath Bhanjan": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 26,
+        'AO Number': 93
+    },
+    "Hapur": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 17,
+        'AO Number': 96
+    },
+    "Etawah": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 80,
+        'AO Number': 93
+    
+    },
+    "Mirzapur-Vindhyachal": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 23,
+        'AO Number': 94
+    
+    },
+    "Bulandshahr": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 17,
+        'AO Number': 95
+    
+    },
+    "Sambhal": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 71,
+        'AO Number': 2
+    },
+    "Hardoi": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 43,
+        'AO Number': 4
+    },
+    "Fatehpur": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 20,
+        'AO Number': 93
+    },
+    "Raebareli": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 61,
+        'AO Number': 94
+    },
+    "Orai": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 54,
+        'AO Number': 5
+    },
+    "Sitapur": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 43,
+        'AO Number': 1
+    },
+    "Bahraich": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 61,
+        'AO Number': 93
+    },
+    "Modinagar": {
+        'Area Code': 'DLC',
+        'AO Type': 'CA',
+        'Range Code': 257,
+        'AO Number': 1
+    },
+    "Unnao": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 91,
+        'AO Number': 92
+    },
+    "Jaunpur": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 61,
+        'AO Number': 4
+    },
+    "Lakhimpur": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 43,
+        'AO Number': 2
+    },
+    "Hathras": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 75,
+        'AO Number': 95
+    },
+    "Banda": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 54,
+        'AO Number': 93
+    },
+    "Pilibhit": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 41,
+        'AO Number': 92
+    },
+    "Barabanki": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 93,
+        'AO Number': 94
+    },
+    "Khurja": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 17,
+        'AO Number': 95
+    },
+    "Gonda": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 61,
+        'AO Number': 96
+    },
+    "Mainpuri": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 85,
+        'AO Number': 94
+    },
+    "Lalitpur": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 80,
+        'AO Number': 95
+    },
+    "Etah": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 75,
+        'AO Number': 92
+    },
+    "Deoria": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 26,
+        'AO Number': 94
+    },
+    "Badaun": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 71,
+        'AO Number': 91
+    },
+    "Ghazipur": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 23,
+        'AO Number': 96
+    },
+    "Sultanpur": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 61,
+        'AO Number': 91
+    },
+    "Azamgarh": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 26,
+        'AO Number': 92
+    },
+    "Bijnor": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 71,
+        'AO Number': 94
+    },
+    "Basti": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 61,
+        'AO Number': 95
+    },
+    "Chandausi": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 71,
+        'AO Number': 93
+    },
+    "Akbarpur": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 61,
+        'AO Number': 3
+    },
+    "Ballia": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 23,
+        'AO Number': 93
+    },
+    "Tanda": {
+        'Area Code': 'LKN',
+        'AO Type': 'W',
+        'Range Code': 61,
+        'AO Number': 3
+    },
+    "Greater Noida": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 100,
+        'AO Number': 1
+    },
+    "Shikohabad": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 80,
+        'AO Number': 91
+    },
+    "Shamli": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 23,
+        'AO Number': 4
+    },
+    "Awagarh": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 75,
+        'AO Number': 92
+    },
+    "Kasganj": {
+        'Area Code': 'KNP',
+        'AO Type': 'W',
+        'Range Code': 75,
+        'AO Number': 94
     }
 }
-
